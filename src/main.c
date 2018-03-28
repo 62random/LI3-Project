4bff85297f299d02299bb79b4d2c7643a3b616eb
Diogo Sobral <diogosobral98@gmail.com>
2018-03-28 23:11:57 +0100

    main-..2 q

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
 //#include <date.h>
 #include <stdio.h>
+#include <myuser.h>
 
 int main(){
 
-        printf("Que cena random\n");
+        GTree * tree = createMYUSERS_TREE("/Users/Ambrosiny/Desktop/Universidade/2ano/LI3/dump exemplo/android/Users.xml");
+
+		long key = 0;
+		MYUSER use;
+
+		while(scanf("%ld",&key)){
+			use = search_USER(tree,key);
+			if (!use)
+				printf("Não encontrou\n");
+			else{
+				printf("ID:%ld\n",getIdMYUSER(use));
+				printf("Rep:%d\n",getREPMYUSER(use));
+				printf("Use:%s\n", getUsername(use));
+				printf("Bio:%s\n",getBiography(use));
+			}
+		}
+
+		freeTreeUSER(tree);
+
+
+
         return 1;
 }
