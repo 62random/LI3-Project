4f3a87d109a70e4c6aad7651e5969bb3a5091644
Diogo Sobral <diogosobral98@gmail.com>
2018-03-28 15:18:37 +0100

    bugs

--- include/mydate.h
+++ include/mydate.h
@@ -14,7 +14,7 @@ void free_MYdate(MYDATE d);
 int MYdate_size();
 int compare_MYDATE_AVL(void * data1, void * data2);
 int compare_MYDATE_LIST(void * data1, void * data2);
-Date MYDATEtoDate(MYDATE a)
+Date MYDATEtoDate(MYDATE a);
 MYDATE DatetoMYDATE(Date a);
 
 #endif
