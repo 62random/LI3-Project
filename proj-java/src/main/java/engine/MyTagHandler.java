2667a6f07210df5fa6eddc73e5ea001aaadef22f
62random <josepedropinto98@gmail.com>
2018-06-09 01:02:41 +0100

    community

---
+++ proj-java/src/main/java/engine/MyTagHandler.java
@@ -0,0 +1,44 @@
+package engine;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.HashMap;
+
+
+
+
+public class MyTagHandler extends DefaultHandler {
+
+    private HashMap<String, Long> tags = null;
+
+    public HashMap<String, Long> getTags() {
+        return tags;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (qName.equalsIgnoreCase("row")) {
+
+            String str1 = attributes.getValue("TagName");
+            String str2 = attributes.getValue("Id");
+
+            if (tags == null)
+                tags = new HashMap<String, Long>();
+
+            if(str1 != null && str2 != null)
+                tags.put(str1, Long.parseLong(str2));
+        }
+
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+    }
+
+    @Override
+    public void characters(char ch[], int start, int length) throws SAXException {
+    }
+}
---
+++ proj-java/src/main/java/engine/MyUserHandler.java
@@ -0,0 +1,78 @@
+package engine;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MyUserHandler extends DefaultHandler {
+
+    private List<MyUser> users = null;
+    private Map<Long, Integer> users_rep = null;
+    private MyUser u = null;
+
+    public List<MyUser> getUsers() {
+        return users;
+    }
+
+    public Map<Long, Integer> getUsers_rep() {
+        return users_rep;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (users == null)
+            users = new LinkedList<MyUser>();
+
+        if (users_rep == null)
+            users_rep = new HashMap<Long, Integer>();
+
+        int n;
+
+        if (qName.equalsIgnoreCase("row")) {
+            u = new MyUser();
+
+            String str = attributes.getValue("Id");
+            if(str != null)
+                u.setId(Long.parseLong(str));
+
+            str = attributes.getValue("DisplayName");
+            if(str != null)
+                u.setUsername(str);
+
+            str = attributes.getValue("Reputation");
+            if(str != null) {
+                n = Integer.parseInt(str);
+                u.setRep(n);
+                users_rep.put(u.getId(), n);
+            }
+
+            str = attributes.getValue("AboutMe");
+            if(str != null)
+                u.setBio(str);
+
+        }
+
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (qName.equalsIgnoreCase("row")) {
+            users.add(u);
+        }
+    }
+
+
+
+    @Override
+    public void characters(char ch[], int start, int length) throws SAXException {
+    }
+
+
+
+}
